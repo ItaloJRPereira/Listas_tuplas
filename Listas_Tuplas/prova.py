5c1c255746f06653a49cc48e263c168b7687cc00
ItaloJRPereira <italo.jose14@hotmail.com>
2025-09-28 23:07:16 -0300

    Update prova.py

--- Listas_Tuplas/prova.py
+++ Listas_Tuplas/prova.py
@@ -1,8 +1,7 @@
 frutas = ["maçã", "banana", "uva", "laranja", "manga", "jaca"]
 
-print("\nLista de frutas:\n", frutas)
-
 # Convertendo a lista em tupla
 frutas_tupla = tuple(frutas)
 
 print("\nTupla de frutas:\n", frutas_tupla)
+
